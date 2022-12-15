0261349c627bd81586ec4aa5c9f6e0165f2b7ffb
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-15 14:37:15 +0300

    example_006 find max

---
+++ example_006/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1; 
+int b = 4;
+int c = 2; 
+int d = 3;
+int e = 6; 
+
+int max = a; 
+if(a > max) max = a; 
+if(b > max) max = b; 
+if(c > max) max = c;
+if(d > max) max = d; 
+if(e > max) max = e; 
+
+Console.Write(max); 
