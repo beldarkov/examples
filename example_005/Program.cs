c7aea5d8f1e07a35081e76c51a7ff6f236dfd3d4
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-15 14:29:05 +0300

    Example_005 with if/else

---
+++ example_005/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите Ваше имя: ");
+string name = Console.ReadLine(); 
+if(name.ToLower() == "маша") {
+    Console.Write("Ура, это " + name);
+} else {
+    Console.Write("Привет, " + name);
+}
